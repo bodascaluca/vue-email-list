ab554eac1209e62da89d49e5cf73c92ef0424344
bodascaluca <bodasca.luca@gmail.com>
2022-05-14 19:22:36 +0200

    Un altra variante per fare l'es dichiarando tutto a crated senza il bottone

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
 
+// const app = new Vue({
+//     el:"#root",
+//     data:{
+//         userEmail:[],
+//     },
+//     mounted(){
+//         this.generateRandomEmails()
+//     },
+//     methods:{
+//         generateRandomEmails(){
+//             console.log('Salve');
+//             for(let i = 0; i < 10; i++){
+//                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//                 .then((resp)=>{ 
+//                     this.userEmail.push(resp.data.response);
+//                 });
+//             }
+
+//         },  
+//     }
+// });
+
 const app = new Vue({
     el:"#root",
     data:{
         userEmail:[],
     },
-    mounted(){
-        this.generateRandomEmails()
-    },
-    methods:{
-        generateRandomEmails(){
-            console.log('Salve');
+    created(){
             for(let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((resp)=>{ 
                     this.userEmail.push(resp.data.response);
-                })
-            }
-
-        },  
-    }
+                });
+            } 
+    },
+    
 });
+
